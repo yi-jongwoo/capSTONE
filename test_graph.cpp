6a584e1d11602df4447eac8cb3ff3fb00be2c827
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2024-05-16 10:04:58 +0900

    Create test_graph.cpp

---
+++ test_graph.cpp
@@ -0,0 +1,11 @@
+#include "stone_internal_header.h"
+
+bool dfs_check_tree(int x,int y,vector<int>* arr,int* vit){
+	
+}
+
+bool is_tree(int n,vector<int>* arr){
+	int*vit=new int[n];
+	dfs_check_tree(0,-1,arr,vit);
+	delete[] vit;
+}
