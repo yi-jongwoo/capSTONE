987732c6bd67de4fea7da2a912abf19e964bef23
yi-jongwoo <82140757+yi-jongwoo@users.noreply.github.com>
2024-05-23 09:40:45 +0900

    Update stone_internal_header.h

--- stone_internal_header.h
+++ stone_internal_header.h
@@ -1,7 +1,11 @@
+#ifndef STONE_INTERNAL_H
+#define STONE_INTERNAL_H
+
 #include <bits/stdc++.h>
 using namespace std;
-#include "../boost_1_82_0/boost/graph/adjacency_list.hpp"
-#include "../boost_1_82_0/boost/graph/boyer_myrvold_planar_test.hpp"
+
+#define NOTHING -1
+#define IMPOSSIBLE -2
 
 //impl in test_graph.cpp
 bool is_tree(int n,vector<int>* arr);
@@ -17,3 +21,5 @@ void embad_general(int n,vector<int>* grr,int w,int h,int* x,int* y);
 //impl in visualization.cpp
 void make_svg_undirected(string filename,int w,int h,int n,vector<int>* arr,int* x,int* y);
 void make_svg(string filename,int w,int h,int n,vector<int>* arr,int* x,int* y);
+
+#endif
